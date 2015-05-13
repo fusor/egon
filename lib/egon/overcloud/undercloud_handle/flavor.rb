f2754b60b4cf62634e479ae74371a786e4a5d951
Tzu-Mainn Chen <tzumainn@redhat.com>
2015-05-13 14:08:53 -0400

    added flavor extra_specs functionality

--- lib/egon/overcloud/undercloud_handle/flavor.rb
+++ lib/egon/overcloud/undercloud_handle/flavor.rb
@@ -8,11 +8,22 @@ module Overcloud
     def get_flavor(flavor_id)
       service('Compute').flavors.get(flavor_id)
     end
-    
+
     def create_flavor(flavor_parameters)
-      service('Compute').flavors.create(flavor_parameters)
+      flavor = service('Compute').flavors.create(flavor_parameters)
+      if flavor_parameters.key?(:extra_specs)
+        create_flavor_extra_specs(flavor.id, flavor_parameters[:extra_specs])
+      end
     end
-    
+
+    def get_flavor_extra_specs(flavor_id)
+      get_flavor(flavor_id).extra_specs
+    end
+
+    def create_flavor_extra_specs(flavor_id, extra_specs)
+      get_flavor(flavor_id).create_extra_specs(extra_specs)
+    end
+        
     def create_flavor_from_node(node)
       cpus = node.properties['cpus']
       memory_mb = node.properties['memory_mb']
@@ -25,6 +36,11 @@ module Overcloud
         :vcpus => cpus,
         :disk => local_gb,
         :is_public => true,
+        :extra_specs => {
+          :cpu_arch => cpu_arch,
+          :'baremetal:deploy_kernel_id' => get_baremetal_deploy_kernel_image.id,
+          :'baremetal:deploy_ramdisk_id' => get_baremetal_deploy_ramdisk_image.id
+        }
       }
       
       if !flavor_exists?(flavor_parameters)
@@ -36,7 +52,8 @@ module Overcloud
       for flavor in list_flavors
         if flavor.ram == flavor_parameters[:ram].to_i &&
             flavor.vcpus == flavor_parameters[:vcpus].to_i &&
-            flavor.disk == flavor_parameters[:disk].to_i
+            flavor.disk == flavor_parameters[:disk].to_i &&
+            flavor.extra_specs['cpu_arch'] == flavor_parameters[:extra_specs][:cpu_arch]
           return true
         end
       end
--- lib/egon/overcloud/undercloud_handle/image.rb
+++ lib/egon/overcloud/undercloud_handle/image.rb
@@ -5,5 +5,16 @@ module Overcloud
       service('Image').images
     end
 
+    def find_image_by_name(image_name)
+      service('Image').images.find{|image| image.name == image_name}
+    end
+
+    def get_baremetal_deploy_kernel_image
+      find_image_by_name('bm-deploy-kernel')
+    end
+
+    def get_baremetal_deploy_ramdisk_image
+      find_image_by_name('bm-deploy-ramdisk')
+    end
   end
 end
